343a72f9f286dc3a96ee602d88f209a205669177
William Nery <william.nery.pereira@gmail.com>
2022-08-02 20:47:10 -0300

    Possibilidade para alteração do nome e ciclo de vida activity

--- app/src/main/java/com/williamnery/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/williamnery/motivation/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.williamnery.motivation.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,7 +24,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         // Esconder a barra de navegação
         supportActionBar?.hide()
-        handleUserName()
         handleFilter(R.id.image_all) // Resetando imagens
         handleNextPhrase()
 
@@ -32,7 +32,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.imageAll.setOnClickListener(this)
         binding.imageHappy.setOnClickListener(this)
         binding.imageSunny.setOnClickListener(this)
+        binding.textUserName.setOnClickListener(this)
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handleUserName()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
     override fun onClick(view: View) {
@@ -40,6 +62,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
            handleNextPhrase()
         } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
             handleFilter(view.id)
+        } else if (view.id == R.id.text_user_name) {
+            SecurityPreferences(this).storeString(MotivationConstants.KEY.USER_NAME, "")
+            startActivity(Intent(this, UserActivity::class.java))
         }
     }
 
