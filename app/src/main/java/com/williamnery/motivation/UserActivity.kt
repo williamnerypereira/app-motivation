3356b73ae09601cf6ef35e4a7d488b0a6cde5b00
William Nery <william.nery.pereira@gmail.com>
2022-08-02 17:04:26 -0300

    Navegação entre activities

--- app/src/main/java/com/williamnery/motivation/UserActivity.kt
+++ app/src/main/java/com/williamnery/motivation/UserActivity.kt
@@ -1,13 +1,16 @@
 package com.williamnery.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.williamnery.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
@@ -19,8 +22,18 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
-        if(v.id == R.id.button_save) {
-            
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Finaliza a tela
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
