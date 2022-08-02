30a095dbd5c9e6df4d016c17397aaf99a07974c1
William Nery <william.nery.pereira@gmail.com>
2022-08-02 18:30:45 -0300

    Padronização e configuração dos icones do menu

--- app/src/main/java/com/williamnery/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/williamnery/motivation/infra/MotivationConstants.kt
@@ -6,4 +6,10 @@ class MotivationConstants private constructor() {
         const val USER_NAME = "USER_NAME"
     }
 
+    object FILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
+    }
+
 }
--- app/src/main/java/com/williamnery/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/williamnery/motivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.williamnery.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.williamnery.motivation.infra.MotivationConstants
 import com.williamnery.motivation.R
 import com.williamnery.motivation.infra.SecurityPreferences
@@ -11,6 +12,7 @@ import com.williamnery.motivation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var categoryId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +23,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // Esconder a barra de navegação
         supportActionBar?.hide()
         handleUserName()
+        handleFilter(R.id.image_all) // Resetando imagens
 
         // Eventos
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
 
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
             var s = ""
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple)) // 1
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple)) // 2
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple)) // 3
+
+        // when tipo switch/case
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
         }
     }
 
@@ -37,4 +67,5 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
         binding.textUserName.text = "Olá, $name!"
     }
+
 }
